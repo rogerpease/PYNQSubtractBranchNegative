5b8e2bba1fc8c3005baf927937904c65fe9cd8b8
Roger Pease <rogerpease@gmail.com>
2021-06-06 17:19:54 -0500

    Fixed bogus rm.

--- RunVerilator.sh
+++ RunVerilator.sh
@@ -3,7 +3,6 @@
 set -e 
 
 cd Verilator
-rm TestSBNDatapath
 rm -rf obj_dir
 verilator --cc --exe --trace --build -j TestSBN.cpp ../Verilog/module/SBNDatapath.v --dump-defines -o TestSBNDatapath
 ./obj_dir/TestSBNDatapath
